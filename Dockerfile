f002f5e04114b2dec684ba6b8b0358d300a95cd5
84TommyJerry84 <tommi84@hotmail.fr>
2026-08-21 16:28:19 +0200

    ajout des deux service dans le compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
 COPY script.py .
 
 CMD ["python", "script.py"]
--- script.py
+++ script.py
@@ -1,3 +1,38 @@
-# script.py
-
 print("Bonjour depuis mon conteneur Docker !")
+
+import psycopg2
+
+
+connection = psycopg2.connect(
+    host="db",
+    database="dockerdb",
+    user="admin",
+    password="admin",
+    port=5432
+)
+
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS batch_test (
+        id SERIAL PRIMARY KEY,
+        message VARCHAR(255)
+    )
+""")
+
+cursor.execute("""
+    INSERT INTO batch_test (message)
+    VALUES (%s)
+""", ("Bonjour depuis le batch Python",))
+
+connection.commit()
+
+cursor.execute("SELECT * FROM batch_test")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+cursor.close()
+connection.close()
